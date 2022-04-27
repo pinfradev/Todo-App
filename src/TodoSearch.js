05e74589ea0baaab51c3b6768b4f5d2082bb17eb
Fray <fraypineda0@gmail.com>
2022-04-26 20:10:26 -0400

    estado en componente Todo search

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,13 +1,24 @@
+import { render } from "@testing-library/react"
 import React from "react"
 import './TodoSearch.css'
+
 function TodoSearch() {
+    const [searchValue, setSearchValue] = React.useState('')
 
     const onSearchValueChange = (event) => {
         console.log(event.target.value)
+        setSearchValue(event.target.value)
     }
-    return (
-        <input onChange={onSearchValueChange} className="TodoSearch" placeholder="cebollas"/>
-    )
+
+    return [
+        <input 
+        onChange={onSearchValueChange}
+        className="TodoSearch" 
+        placeholder="cebollas"
+        value={searchValue}
+        />,
+        <p>{searchValue}</p>
+    ]
 }
 
 export {TodoSearch}
